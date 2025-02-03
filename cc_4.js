842902b3942e6ecd42cf1053dafb8081a899af36
Markayla Montgomery <mm42@usf.edu>
2025-02-02 19:40:38 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -10,3 +10,17 @@ if (purchaseAmount > 100) {
 }
 // Log the final amount to the console using a template literal
 console.log(`Final amount after discount: $${finalAmount}`);
+
+// Task 2: For Loop
+// Declare an array sales with at least five sales figures
+let sales = [250, 400, 300, 150, 500];
+console.log("Sales Array:", sales);
+// Use a while loop to decrease stock until it reaches zero
+let totalSales = 0;
+let i = 0;
+while (i < sales.length) {
+    totalSales += sales[i];
+    i++;
+}
+// Log each decrement to the console using a template literal
+console.log(`Total sales: $${totalSales}`);
